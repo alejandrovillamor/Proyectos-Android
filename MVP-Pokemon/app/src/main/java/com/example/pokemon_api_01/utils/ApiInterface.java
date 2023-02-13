50b87366ec4d32e1fb8b0d9411399b9798d4c4ec
alejandrovillamor <122881552+alejandrovillamor@users.noreply.github.com>
2023-02-13 12:06:54 +0100

    preparando

---
+++ MVP-Pokemon/app/src/main/java/com/example/pokemon_api_01/utils/ApiInterface.java
@@ -0,0 +1,27 @@
+package com.example.pokemon_api_01.utils;
+
+import com.example.pokemon_api_01.entities.Pokemon;
+import com.example.pokemon_api_01.entities.PokemonRespuesta;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+    @GET("pokemon")
+    Call<ArrayList<Pokemon>> getPokemons();
+
+    @GET("pokemon")
+    Call<PokemonRespuesta> getPokemons(@Query("limit") int limit, @Query("offset") int offset);
+
+
+    //https://pokeapi.co/api/v2/pokemon?limit=20&offset=20
+    //Call<ArrayList<Simpson>>  getPopularSimpson(@Path("count") int count);
+/*
+    @GET("movie/{movie_id}")
+    Call<Movie> getMovieDetails(@Path("movie_id") int movieId,
+                @Query("api_key") String apiKey, @Query("append_to_response") String credits);
+*/
+}
